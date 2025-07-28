e83d88b5385df7219d68e68a1a37901f583ad8ee
luhevapradnya <evapradnyaningsih54@gmail.com>
2025-07-28 19:03:44 +0700

    Update dashboard_kur.py

--- pages/dashboard_kur.py
+++ pages/dashboard_kur.py
@@ -18,18 +18,19 @@ st.markdown(no_sidebar_style, unsafe_allow_html=True)
 
 with st.sidebar:
     st.image("https://upload.wikimedia.org/wikipedia/commons/9/97/Logo_BRI.png", width=200)
-    selected = option_menu(
-        menu_title="Navigasi",
+    selected = st.radio(
+        "Navigasi",
         options=["Beranda", "Data Entry", "Dashboard"],
-        icons=["house", "pencil-square", "bar-chart"],
-        menu_icon="cast",
-        default_index=2
+        index=0,
+        format_func=lambda x: f"📌 {x}" if x == "Beranda" else ("📊 " + x if x == "Dashboard" else "📝 " + x)
     )
 
+# Content based on selected menu
 if selected == "Beranda":
-    st.switch_page("main_app.py")
-elif selected == "Data Entry":
-    st.switch_page("pages/entry_mapping.py")
+    st.markdown("<h1 style='text-align: center; color: #000000; font-weight: 900;'>Aplikasi Pendataan Kredit UMKM</h1>", unsafe_allow_html=True)
+    st.markdown("<h3 style='text-align: center; color: #0033EE; font-weight: 700;'>Bank Rakyat Indonesia (BRI)</h3>", unsafe_allow_html=True)
+    st.markdown("---")
+
 
 # Fungsi load data
 def load_data():
